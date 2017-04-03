435663bcec6087b600c26fbc620acf3b8f52ec2a
Jamin King <jamintking@gmail.com>
2017-04-03 08:43:31 -0700

    - Implemented a O(n^2) solution to Gaming Array. Times out on last few HR test cases. - Built test cases to drive development of a solution with better run-time complexity. - TODO: Decrease run-time complexity.

---
+++ HackerRankSolutions/GamingArray/GamingArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.GamingArray
+{
+    /// <summary>
+    /// Provides solution to Gamer Array problem on HackerRank.
+    /// See https://www.hackerrank.com/challenges/an-interesting-game-1.
+    /// </summary>
+    public static class GamingArray
+    {
+        public const string PlayerOne = "BOB";
+
+        public const string PlayerTwo = "ANDY";
+
+        public static string WhoWins(int[] board) => CurrentPlayerWins(board) ? PlayerOne : PlayerTwo;
+
+        /// <summary>
+        /// O(n^2) solution
+        /// </summary>
+        /// <param name="board">Board setup on current player's turn</param>
+        /// <returns>Whether the current player wins</returns>
+        private static bool CurrentPlayerWins(int[] board)
+        {
+            if(board.Length == 0)
+                return false;
+            int maxIndex = Array.IndexOf(board, board.Max());
+            board = board.Take(maxIndex).ToArray();
+            return !CurrentPlayerWins(board);
+        }
+    }
+}
---
+++ HackerRankSolutions/GamingArray/GamingArray_Test.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace HackerRankSolutions.GamingArray
+{
+    [TestClass]
+    public class GamingArray_Test
+    {
+        [TestMethod]
+        public void SampleInput_0_Line0()
+        {
+            int[] board = { 5, 2, 6, 3, 4 };
+            Assert.AreEqual(GamingArray.PlayerTwo, GamingArray.WhoWins(board));
+        }
+
+        [TestMethod]
+        public void SampleInput_0_Line1()
+        {
+            int[] board = { 3, 1 };
+            Assert.AreEqual(GamingArray.PlayerOne, GamingArray.WhoWins(board));
+        }
+
+        [TestMethod]
+        public void EmptyBoard()
+        {
+            int[] board = { };
+            Assert.AreEqual(GamingArray.PlayerTwo, GamingArray.WhoWins(board));
+        }
+
+        [TestMethod]
+        public void SingleElement()
+        {
+            int[] board = { 1 };
+            Assert.AreEqual(GamingArray.PlayerOne, GamingArray.WhoWins(board));
+        }
+
+        [TestMethod]
+        public void TwoOfSameElement()
+        {
+            int[] board = { 2, 2 };
+            Assert.AreEqual(GamingArray.PlayerOne, GamingArray.WhoWins(board));
+        }
+
+        [TestMethod]
+        public void TwoElementsSecondLarger()
+        {
+            int[] board = { 1, 2 };
+            Assert.AreEqual(GamingArray.PlayerTwo, GamingArray.WhoWins(board));
+        }
+
+        [TestMethod]
+        public void GeneralAscendingOrder()
+        {
+            for(int i = 0; i < 10; ++i)
+            {
+                int[] board = Enumerable.Range(0, i).ToArray();
+                string expectedWinner = i % 2 == 1 ? GamingArray.PlayerOne : GamingArray.PlayerTwo;
+                Assert.AreEqual(expectedWinner, GamingArray.WhoWins(board));
+            }
+        }
+
+        [TestMethod]
+        public void TestCase_16()
+        {
+            string filepath = Path.Combine
+                (
+                    Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName,
+                    "RunningMedian",
+                    "TestCaseInput",
+                    "TestCase_16_Input.txt"
+                );
+            string[] lines = File.ReadAllLines(filepath);
+            int[][] inputs = lines.Skip(1)
+                .Where((s, i) => i % 2 == 1)
+                .Select((s) => Array.ConvertAll(s.Split(' '), int.Parse))
+                .ToArray();
+            string[] expectedOutputs =
+            {
+                "BOB",
+                "BOB",
+                "BOB",
+                "ANDY",
+                "BOB",
+                "BOB",
+                "BOB",
+                "BOB",
+                "ANDY",
+                "BOB"
+            };
+            for(int i = 0; i < inputs.Length; ++i)
+            {
+                Assert.AreEqual(expectedOutputs[i], GamingArray.WhoWins(inputs[i]));
+            }
+        }
+    }
+}

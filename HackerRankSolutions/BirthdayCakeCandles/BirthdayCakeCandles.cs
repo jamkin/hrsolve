7637addc3701d41cddb73bd1b3ef909f9e630694
Jamin King <jamintking@gmail.com>
2017-04-04 07:41:01 -0700

    - Solved Birthday Cake Candles problem (super easy)

---
+++ HackerRankSolutions/BirthdayCakeCandles/BirthdayCakeCandles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.BirthdayCakeCandles
+{
+    /// <summary>
+    /// Solution to https://www.hackerrank.com/challenges/birthday-cake-candles
+    /// </summary>
+    public static class BirthdayCakeCandles
+    {
+        /// <summary>
+        /// The problem reduces to finding the number of candles with heights
+        /// equal to the max height.
+        /// 
+        /// Time-Complexity: O(n) where n = heights.Length
+        /// </summary>
+        public static int CandlesToBlow(int[] heights)
+        {
+            // Note: We could do this with 1 pass through the array by keeping track
+            // of the max and grouping the heights. But this looks better.
+            if(heights.Length == 0)
+                return 0;
+            int max = heights.Max();
+            return heights.Count(h => h == max);
+        }
+    }
+}
---
+++ HackerRankSolutions/BirthdayCakeCandles/BirthdayCakeCandles_Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.BirthdayCakeCandles
+{
+    [TestClass]
+    public class BirthdayCakeCandles_Test
+    {
+       [TestMethod]
+        public void EmptyInput()
+        {
+            int[] heights = { };
+            Assert.AreEqual(0, BirthdayCakeCandles.CandlesToBlow(heights));
+        }
+
+        [TestMethod]
+        public void OneCandle()
+        {
+            int[] heights = { 56 };
+            Assert.AreEqual(1, BirthdayCakeCandles.CandlesToBlow(heights));
+        }
+
+        [TestMethod]
+        public void TwoCandlesSecondTallerThanFirst()
+        {
+            int[] heights = { 1, 2 };
+            Assert.AreEqual(1, BirthdayCakeCandles.CandlesToBlow(heights));
+        }
+
+        [TestMethod]
+        public void TwoCandlesFirstTallerThanSecond()
+        {
+            int[] heights = { 2, 1 };
+            Assert.AreEqual(1, BirthdayCakeCandles.CandlesToBlow(heights));
+        }
+
+        [TestMethod]
+        public void TwoOfSameHeight()
+        {
+            int[] heights = { 1, 1 };
+            Assert.AreEqual(2, BirthdayCakeCandles.CandlesToBlow(heights));
+        }
+
+        [TestMethod]
+        public void ThreeCandlesOneSmallerHeight()
+        {
+            int[] heights = { 1, 2, 2 };
+            Assert.AreEqual(2, BirthdayCakeCandles.CandlesToBlow(heights));
+        }
+
+        [TestMethod]
+        public void SampleInput_0()
+        {
+            int[] heights = { 3, 2, 1, 3 };
+            Assert.AreEqual(2, BirthdayCakeCandles.CandlesToBlow(heights));
+        }
+    }
+}

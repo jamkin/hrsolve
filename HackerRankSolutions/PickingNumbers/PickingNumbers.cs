8a112a7f108c130c37859df43907b0cf7a1c3afd
Jamin King <jamintking@gmail.com>
2017-04-26 23:21:13 -0700

    - Created solution for Picking Numbers problem - Added unit tests

---
+++ HackerRankSolutions/PickingNumbers/PickingNumbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Provides a solution to the Picking Numbers problem
+/// https://www.hackerrank.com/challenges/picking-numbers?h_r=next-challenge&h_v=zen
+/// </summary>
+namespace HackerRankSolutions.PickingNumbers
+{
+    public static class PickingNumbers
+    {
+        /// <summary>
+        /// O(n) algorithm for finding the largest subarray such that any 2
+        /// elements are less than or equal to distance 1 from each other.
+        /// Validates input.
+        /// </summary>
+        public static int Solve(int[] arr)
+        {
+            if(arr.Length < 2 || arr.Length > 100)
+                throw new ArgumentException(nameof(arr));
+
+            // Store the counts of each numbers: 
+            // counts[i - 1] is the count of i
+            int[] counts = new int[99];
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                if(arr[i] <= 0 || arr[i] >= 100)
+                    throw new ArgumentException($"arr[{i}]");
+                ++counts[arr[i] - 1];            
+            }
+            // Find the maximum combined counts of number i and i + 1. 
+            // This is easy with our lookup array.
+            int max = 0;
+            for(int i = 1; i < counts.Length; ++i)
+            {
+                int sum = counts[i - 1] + counts[i];   
+                if(sum > max)
+                {
+                    max = sum;
+                }
+            } 
+            return max;
+        }
+    }
+}
---
+++ HackerRankSolutions/PickingNumbers/PickingNumbers_Test.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HackerRankSolutions.PickingNumbers
+{
+    [TestClass]
+    public class PickingNumbers_Test
+    {
+        [TestMethod]
+        public void SampleInput_0()
+        {
+            int[] arr = { 4, 6, 5, 3, 3, 1 };
+            Assert.AreEqual(3, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void SampleInput_1()
+        {
+            int[] arr = { 1, 2, 2, 3, 1, 2 };
+            Assert.AreEqual(5, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ArrayTooShort_0()
+        {
+            int[] arr = { };
+            PickingNumbers.Solve(arr);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ArrayTooShort_1()
+        {
+            int[] arr = { 5 };
+            PickingNumbers.Solve(arr);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ARrayTooLong()
+        {
+            int[] arr = Enumerable.Repeat(5, 101).ToArray();
+            PickingNumbers.Solve(arr);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ElementTooSmall()
+        {
+            int[] arr = { 4, 0, 3 };
+            PickingNumbers.Solve(arr);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ElementTooLarge()
+        {
+            int[] arr = { 4, 100, 3 };
+            PickingNumbers.Solve(arr);
+        }
+
+        [TestMethod]
+        public void InputOrderDoesntMatter()
+        {
+            for(int t = 0; t < 100; ++t)
+            {
+                int[] forward = GenerateRandomInput();
+                int[] backward = GenerateRandomInput();
+                Assert.AreEqual(PickingNumbers.Solve(forward), PickingNumbers.Solve(backward));
+            }
+        }
+
+        [TestMethod]
+        public void MiscTest_0()
+        {
+            int[] arr = { 1, 1 };
+            Assert.AreEqual(2, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_1()
+        {
+            int[] arr = { 1, 2 };
+            Assert.AreEqual(2, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_2()
+        {
+            int[] arr = { 1, 3 };
+            Assert.AreEqual(1, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_3()
+        {
+            int[] arr = { 1, 1, 1 };
+            Assert.AreEqual(3, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_4()
+        {
+            int[] arr = { 1, 1, 2 };
+            Assert.AreEqual(3, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_5()
+        {
+            int[] arr = { 1, 2, 2 };
+            Assert.AreEqual(3, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_6()
+        {
+            int[] arr = { 1, 2, 4 };
+            Assert.AreEqual(2, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_7()
+        {
+            int[] arr = { 1, 3, 4 };
+            Assert.AreEqual(2, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_8()
+        {
+            int[] arr = { 1, 3, 4 };
+            Assert.AreEqual(2, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_9()
+        {
+            int[] arr = { 1, 3, 5 };
+            Assert.AreEqual(1, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_10()
+        {
+            int[] arr = { 1, 3, 4, 10, 1, 3, 10, 9, 67, 5 };
+            Assert.AreEqual(3, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void MiscTest_11()
+        {
+            int[] arr = { 1, 3, 4, 10, 1, 3, 10, 9, 67, 5, 4 };
+            Assert.AreEqual(4, PickingNumbers.Solve(arr));
+        }
+
+        [TestMethod]
+        public void CompareAgainstBruteForce()
+        {
+            for(int t = 0; t < 500; ++t)
+            {
+                int[] arr = GenerateRandomInput();
+                Assert.AreEqual(PickingNumbers.Solve(arr), AltSolve(arr), $"Failed for array {string.Join(",", arr)}");
+            }
+        }
+
+        /// <summary>
+        /// Helper method for generating a random array that statisfieds the problem's constraints
+        /// </summary>
+        private static int[] GenerateRandomInput()
+        {
+            Random r = new Random();
+            int[] arr = new int[r.Next() % 11 + 2];
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = r.Next() % 15 + 1;
+            }
+            return arr;
+        }
+
+        /// <summary>
+        /// Alternative, brute force solution
+        /// </summary>
+        private static int AltSolve(int[] arr)
+        {
+            Array.Sort(arr);
+            return AllSubarrays(arr)
+                .Where(a => (a[a.Length - 1] - a[0]) <= 1)
+                .Max(a => a.Length);
+        }
+  
+        /// <summary>
+        /// Helper method for yielding all contiguous subarrays
+        /// </summary>
+        private static IEnumerable<int[]> AllSubarrays(int[] arr)
+        {
+            for(int size = 1; size <= arr.Length; ++size)
+                for(int i = 0, j = size - 1; j < arr.Length; ++i, ++j)
+                    yield return arr.Skip(i).Take(size).ToArray();
+        }
+    }
+}

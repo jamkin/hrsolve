5e1d3e60a12e3c642dd7c55369c8fc92735e5709
Jamin King <jamintking@gmail.com>
2017-05-01 08:30:23 -0700

    - Began implementing utility class Algorithm. - Wrote tests for one of the two methods implemented. The other   still needs tested.

---
+++ HackerRankSolutions/Algorithm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    /// <summary>
+    /// Utility class to use in solutions and unit testing.
+    /// </summary>
+    public static class Algorithm
+    {
+        /// <summary>
+        /// Iterative algorithm to yield all combinations of a specified size from a collection source. For example, if
+        /// source = { 1, 2, 3, 4 }
+        /// and 
+        /// size = 2
+        /// then the combinations yielded are
+        /// { 1, 1 }
+        /// { 1, 2 }
+        /// { 1, 3 }
+        /// { 1, 4 }
+        /// { 2, 1 }
+        /// { 2, 2 }
+        /// { 2, 3 }
+        /// { 2, 4 }
+        /// { 3, 1 }
+        /// { 3, 2 }
+        /// { 3, 3 }
+        /// { 3, 4 }
+        /// { 4, 1 }
+        /// { 4, 2 }
+        /// { 4, 3 }
+        /// { 4, 4 }.
+        /// The complexity is O(n^s) where n is the size of the source and s is the size of the combinations.
+        /// </summary>
+        public static IEnumerable<IEnumerable<T>> CombinationsOfSize<T>(this IEnumerable<T> source, int size)
+        {
+            if(size < 0)
+                throw new ArgumentException(nameof(source));
+            if(size == 0)
+                yield break;
+            IEnumerable<List<T>> ones = source.Select(t => new List<T>() { t });
+            var combos = new List<IEnumerable<T>>(ones);
+            for(int i = 2; i <= size; ++i)
+            {
+                var nexts = new List<IEnumerable<T>>();
+                foreach(IEnumerable<T> sub in combos)
+                {
+                    foreach(T element in source)
+                    {
+                        var addition = sub.Concat(new List<T>() { element });
+                        nexts.Add(addition);
+                    }
+                }
+                combos = nexts;
+            }
+            foreach(var item in combos)
+                yield return item;
+        }
+
+        /// <summary>
+        /// Yields all subcollections of a source collection. In other words, all subsets of a set
+        /// if we're considering the source set to not necessarily be unique elements (as the typical
+        /// definition of a set would assume). 
+        /// For example, if the source is { 1, 2, 3 } then the results yielded are
+        /// {  },
+        /// { 1 }, 
+        /// { 2 }, 
+        /// { 3 },
+        /// { 1, 2 },
+        /// { 2, 3 },
+        /// { 1, 3 },
+        /// { 1, 2, 3 }
+        /// The time-complexity is O(2^n) where n is the size of the source.
+        /// </summary
+        public static IEnumerable<IEnumerable<T>> AllSubcollections<T>(this IEnumerable<T> source)
+        {
+            // The empty set is always a subcollection
+            yield return new List<T>() { };
+                     
+            T[] arr = source.ToArray();
+            IEnumerable<int> indices = Enumerable.Range(0, arr.Length);
+            var last = new List<HashSet<int>>(new List<HashSet<int>>() { new HashSet<int>() });
+            for(int k = 1; k < arr.Length; ++k)
+            {
+                var next = new List<HashSet<int>>(new List<HashSet<int>>());
+                foreach(HashSet<int> hs in last)
+                {
+                    foreach(int index in indices)
+                    {
+                        if(!hs.Contains(index))
+                        {
+                            var addition = hs.Concat(new List<int> { index });
+                            yield return addition.Select(i => arr[i]);
+                            next.Add(new HashSet<int>(addition));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HackerRankSolutions/Algorithm_Test.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankSolutions
+{
+    /// <summary>
+    /// Test class for our all-encompasing utility class for common algorithms that
+    /// .NET does not have a built-in method for running.
+    /// </summary>
+    [TestClass]
+    public class Algorithm_Test
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CombinationsOfSize_InvalidInput()
+        {
+            int[] arr = { 56, 8 };
+            arr.CombinationsOfSize(-1);
+        }
+
+        [TestMethod]
+        public void CombinationsOfSize_EmptyInput()
+        {
+            int[] arr = { };
+            var result = arr.CombinationsOfSize(0);
+            Assert.IsFalse(result.Any());
+            result = arr.CombinationsOfSize(1);
+            Assert.IsFalse(result.Any());
+        }
+
+        [TestMethod]
+        public void CombinationsOfSize_OneElement()
+        {
+            int[] arr = { 1 };
+            var result = arr.CombinationsOfSize(1);
+            Assert.AreEqual(1, result.Single().Single());
+            result = arr.CombinationsOfSize(2);
+            CollectionAssert.AreEqual(new List<int>() { 1, 1 }, result.Single().ToList());
+        }
+
+        [TestMethod]
+        public void CombinationsOfSize_TwoElements()
+        {
+            int[] arr = { 1, 2 };
+            var result = arr.CombinationsOfSize(1);
+            ExecuteNestedCollectionsEqual(result, new List<List<int>>() { new List<int>() { 1 }, new List<int>() { 2 } });
+            result = arr.CombinationsOfSize(2);
+            ExecuteNestedCollectionsEqual(result, new List<List<int>>() { new List<int>() { 1, 1 }, new List<int>() { 1, 2 }, new List<int>() { 2, 2 }, new List<int>() { 2, 1 } });
+        }
+
+        [TestMethod]
+        public void CombinationsOfSize_ThreeElementsSizeThree()
+        {
+            int[] arr = { 1, 2, 3 };
+            var result = arr.CombinationsOfSize(3);
+            ExecuteNestedCollectionsEqual
+            (
+                new List<List<int>>()
+                {
+                    new List<int>() { 1, 1, 1 }, 
+                    new List<int>() { 1, 2, 1 },
+                    new List<int>() { 1, 1, 2 },
+                    new List<int>() { 1, 2, 2 },
+                    new List<int>() { 1, 3, 2 },
+                    new List<int>() { 1, 2, 3 },
+                    new List<int>() { 1, 3, 3 },
+                    new List<int>() { 1, 1, 3 },
+                    new List<int>() { 1, 3, 1 },
+
+                    new List<int>() { 2, 1, 1 },
+                    new List<int>() { 2, 1, 2 },
+                    new List<int>() { 2, 2, 2 },
+                    new List<int>() { 2, 2, 1 },
+                    new List<int>() { 2, 1, 3 },
+                    new List<int>() { 2, 3, 1 },
+                    new List<int>() { 2, 3, 3 },
+                    new List<int>() { 2, 2, 3 },
+                    new List<int>() { 2, 3, 2 },
+
+                    new List<int>() { 3, 1, 1 },
+                    new List<int>() { 3, 1, 2 },
+                    new List<int>() { 3, 2, 1 },
+                    new List<int>() { 3, 2, 2 },
+                    new List<int>() { 3, 1, 3 },
+                    new List<int>() { 3, 3, 1 },
+                    new List<int>() { 3, 3, 3 },
+                    new List<int>() { 3, 2, 3 },
+                    new List<int>() { 3, 3, 2 },
+                },
+                result
+            );
+        }
+
+        [TestMethod]
+        public void CombinationsOfSize_CorrectResultSize()
+        {
+            int[] input = { 1, 1 };
+            for(int i = 0; i <= 10; ++i)
+            {
+                int expectedSize = i == 0 ? 0 : (int)Math.Pow(input.Length, i);
+                int actualSize = input.CombinationsOfSize(i).Count();
+                Assert.AreEqual(expectedSize, actualSize);
+            }
+        }
+
+        private static void ExecuteNestedCollectionsEqual(IEnumerable<IEnumerable<int>> first, IEnumerable<IEnumerable<int>> second)
+        {
+            string[] s1 = first.Select(c => string.Join(",", c)).ToArray(),
+                     s2 = second.Select(c => string.Join(",", c)).ToArray();
+            Array.Sort(s1);
+            Array.Sort(s2);
+            CollectionAssert.AreEqual(s1, s2);
+        }
+
+        private static IEnumerable<int> GenerateRandomCollection(int maxSize = int.MaxValue, int valMin = int.MinValue, int valMax = int.MaxValue)
+        {
+            Random r = new Random();
+            for(int i = 0; i <= maxSize; ++i)
+                yield return r.Next(valMin, valMax);
+        }
+    }
+}

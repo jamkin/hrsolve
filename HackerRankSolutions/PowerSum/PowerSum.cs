1aeb3c4a5dab2977f987b7ef5b531f8f36b0acbc
Jamin King <jamintking@gmail.com>
2017-04-30 17:04:57 -0700

    - Provided a solution to the Power Sum problem. It is not efficient, but   it passes the performance constraints in the submission. - Wrote tests, but skimped.

---
+++ HackerRankSolutions/PowerSum/PowerSum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankSolutions.PowerSum
+{
+    /// <summary>
+    /// Provides a solution to https://www.hackerrank.com/challenges/the-power-sum/forum
+    /// </summary>
+    public static class PowerSum
+    {
+        /// <summary>
+        /// Bounds of value X in the problem
+        /// </summary>
+        public static readonly Tuple<int, int> XRange = Tuple.Create(1, 1000);
+
+        public static int Solve(int X, int N)
+        {
+            int[] powers = GetPowers(N).ToArray();
+            return SubsetSums(powers, X);
+        }
+
+        /// <summary>
+        /// Ripped from http://stackoverflow.com/questions/18305843/find-all-subsets-that-sum-to-a-particular-value, 
+        /// this is an O(n^2) way of finding all subsets of an array that add up to a particular value.
+        /// </summary>
+        private static int SubsetSums(int[] numbers, int sum)
+        {
+            int[] dp = new int[sum + 1];
+            dp[0] = 1;
+            int currentSum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                currentSum += numbers[i];
+                for(int j = Math.Min(sum, currentSum); j >= numbers[i]; j--)
+                    dp[j] += dp[j - numbers[i]];
+            }
+            return dp[sum];
+        }
+
+        /// <summary>
+        /// Yields all the natural numbers that when raised to the power of n
+        /// fall within the bounds of X
+        /// </summary>
+        private static IEnumerable<int> GetPowers(int n)
+        {
+            for(int b = XRange.Item1; ; ++b)
+            {
+                int p = (int)Math.Pow(b, n);
+                if(p >= XRange.Item1 && p <= XRange.Item2)
+                    yield return p;
+                else
+                    yield break;
+            }
+        }
+    }
+}
---
+++ HackerRankSolutions/PowerSum/PowerSum_Test.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace HackerRankSolutions.PowerSum
+{
+    [TestClass]
+    public class PowerSum_Test
+    {
+        [TestMethod]
+        public void SampleInput_0()
+        {
+            int X = 10, N = 2;
+            Assert.AreEqual(1, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void TestCase_1()
+        {
+            int X = 100, N = 2;
+            Assert.AreEqual(3, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void TestCase_2()
+        {
+            int X = 100, N = 3;
+            Assert.AreEqual(1, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void BaseCase()
+        {
+            int X = 1, N = 2;
+            Assert.AreEqual(1, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void MistTest_0()
+        {
+            int X = 1, N = 3;
+            Assert.AreEqual(1, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void MistTest_1()
+        {
+            int X = 2, N = 2;
+            Assert.AreEqual(0, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void MistTest_2()
+        {
+            int X = 2, N = 3;
+            Assert.AreEqual(0, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void MistTest_3()
+        {
+            int X = 2, N = 4;
+            Assert.AreEqual(0, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void MistTest_4()
+        {
+            int X = 3, N = 2;
+            Assert.AreEqual(0, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void MistTest_5()
+        {
+            int X = 2, N = 5;
+            Assert.AreEqual(0, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void MistTest_6()
+        {
+            int X = 5, N = 2;
+            Assert.AreEqual(1, PowerSum.Solve(X, N));
+        }
+
+        [TestMethod]
+        public void MistTest_7()
+        {
+            int X = 9, N = 3;
+            Assert.AreEqual(1, PowerSum.Solve(X, N));
+        }
+
+        /// <summary>
+        /// Helper method for generating random input that satisfies the problem constraints
+        /// </summary>
+        private static void GenerateRandomInput(out int X, out int N)
+        {
+            Random r = new Random();
+            X = r.Next() % 1000 + 1;
+            N = r.Next() % 9 + 2;
+        }
+    }
+}

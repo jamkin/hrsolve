08fce7ea515fd406360c3ec61b2824ba7d78f535
Jamin King <jamintking@gmail.com>
2017-05-12 17:07:24 -0700

    - Wrote solution to and some unit tests for HackerRank In a String

---
+++ HackerRankSolutions/HackerRankInAString/HackerRankInASring_Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.HackerRankInAString
+{
+    [TestClass]
+    public class HackerRankInASring_Test
+    {
+        [TestMethod]
+        public void EmptyStringTest()
+        {
+            string str = string.Empty;
+            Assert.AreEqual("NO", HackerRankInAString.Solve(str));
+        }
+
+        [TestMethod]
+        public void  NotEmptyButTooSmallTest()
+        {
+            string str = "alkdj";
+            Assert.AreEqual("NO", HackerRankInAString.Solve(str));
+        }
+
+        [TestMethod]
+        public void SameSizeButDifferentCharactersTest()
+        {
+            string str = "habkerrark";
+            Assert.AreEqual("NO", HackerRankInAString.Solve(str));
+        }
+
+        [TestMethod]
+        public void LongerAndDoesntContainTest()
+        {
+            string str = "habkerrarkakdh";
+            Assert.AreEqual("NO", HackerRankInAString.Solve(str));
+        }
+
+        [TestMethod]
+        public void ExactMatchTest()
+        {
+            string str = HackerRankInAString.Target;
+            Assert.AreEqual("YES", HackerRankInAString.Solve(str));
+        }
+
+        [TestMethod]
+        public void IsSubstringTest()
+        {
+            string str = $"xx{HackerRankInAString.Target}x";
+            Assert.AreEqual("YES", HackerRankInAString.Solve(str));
+        }
+
+        [TestMethod]
+        public void CharactersInBetweenTest()
+        {
+            string str = "hxaxckerranxk";
+            Assert.AreEqual("YES", HackerRankInAString.Solve(str));
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankInAString/HackerRankInAString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.HackerRankInAString
+{
+    public static class HackerRankInAString
+    {
+        public const string Target = "hackerrank";
+
+        public static string Solve(string s)
+        {
+            int j = 0;
+            for(int i = 0; i < s.Length && j < Target.Length; ++i)
+            {
+                if(s[i] == Target[j])
+                {
+                    ++j;
+                }
+            }
+            return j == Target.Length ? "YES" : "NO";
+        }
+    }
+}

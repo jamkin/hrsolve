f4bf77f40ce33529f6574814d7ccc68c67b370e6
Jamin King <jamintking@gmail.com>
2017-05-15 07:43:36 -0700

    - Wrote solution to Duplication problem along with unit tests. - TODO: Organize rest of solutions into directories, like I did with this one.

---
+++ HackerRankSolutions/WeekOfCode32/Duplication/Duplication.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.WeekOfCode32.Duplication
+{
+    /// <summary>
+    // Provides a solution to https://www.hackerrank.com/contests/w32/challenges/duplication
+    /// </summary>
+    public static class Duplication
+    {
+        public const int Size = 1024;
+
+        private static byte[] _bits;
+
+        public static byte[] Bits
+        {
+            get
+            {
+                if(_bits == null)
+                    SetBits();
+                return _bits;
+            }
+            private set
+            {
+                _bits = value;
+            }
+        }
+
+        /// <summary>
+        /// Generates the byte array 0110100110010110... described in the prompt.
+        /// </summary>
+        private static void SetBits()
+        {
+            _bits = new byte[Size];
+            _bits[0] = 0;
+            for(int i = 1, j = 2; j <= _bits.Length; i <<= 1, j <<= 1)
+            {
+                for(int k = i; k < j; ++k)
+                {
+                    _bits[k] = (byte)(1 - _bits[k - i]);
+                }
+            }
+        }
+    }
+}
---
+++ HackerRankSolutions/WeekOfCode32/Duplication/Duplication_Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HackerRankSolutions.WeekOfCode32.Duplication
+{
+    [TestClass]
+    public class Duplication_Test
+    {
+        [TestMethod]
+        public void SizeCheck()
+        {
+            Assert.AreEqual(Duplication.Size, Duplication.Bits.Length);
+        }
+
+        [TestMethod]
+        public void SimpleElementCheck()
+        {
+            // 0110100110010110...
+            Assert.AreEqual(0, Duplication.Bits[0]);
+            Assert.AreEqual(1, Duplication.Bits[1]);
+            Assert.AreEqual(1, Duplication.Bits[2]);
+            Assert.AreEqual(0, Duplication.Bits[3]);
+            Assert.AreEqual(1, Duplication.Bits[4]);
+            Assert.AreEqual(0, Duplication.Bits[5]);
+            Assert.AreEqual(0, Duplication.Bits[6]);
+            Assert.AreEqual(1, Duplication.Bits[7]);
+        }
+
+        [TestMethod]
+        public void CompareToAlt()
+        {
+            byte[] alt = BuildAlternatively();
+            CollectionAssert.AreEqual(alt, Duplication.Bits);
+        }
+
+        private static byte[] BuildAlternatively()
+        {
+            byte[] accum = { 0 };
+            while(accum.Length <= 1000)
+            {
+                IEnumerable<byte> next = accum.Select(b => (byte)(1 - b));
+                accum = accum.Concat(next).ToArray();
+            }
+            return accum;
+        }
+    }
+}

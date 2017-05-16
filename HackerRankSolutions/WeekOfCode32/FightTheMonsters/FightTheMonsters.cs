db10259ea7ebf370060418353ba0f56b4b3aeed4
Jamin King <jamintking@gmail.com>
2017-05-16 00:47:24 -0700

    - Wrote solution to, and unit tests for, Fight the Monsters

---
+++ HackerRankSolutions/WeekOfCode32/FightTheMonsters/FightTheMonsters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.WeekOfCode32.FightTheMonsters
+{
+    /// <summary>
+    /// Provides a solution to https://www.hackerrank.com/contests/w32/challenges/fight-the-monsters
+    /// </summary>
+    public static class FightTheMonsters
+    {
+        /// <summary>
+        /// Solves the problem prompt in O(n*log(n)) time where n is h.Length
+        /// </summary>
+        /// <param name="h">Monster healths</param>
+        /// <param name="hit">Hit value</param>
+        /// <param name="t">Time</param>
+        public static int KillCount(int[] h, int hit, int t)
+        {
+            Array.Sort(h);
+            Queue<int> q = new Queue<int>(h);
+            int kills = 0;
+            for (; t > 0 && q.Count > 0;)
+            {
+                int m = q.Dequeue();
+                for(; t > 0 && m > 0; --t, m -= hit) ;
+                if (m <= 0)
+                {
+                    ++kills;
+                }
+            }
+            return kills;
+        }
+    }
+}
---
+++ HackerRankSolutions/WeekOfCode32/FightTheMonsters/FightTheMonsters_Test.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.WeekOfCode32.FightTheMonsters
+{
+    [TestClass]
+    public class FightTheMonsters_Test
+    {
+        [TestMethod]
+        public void NoMonsters()
+        {
+            int[] h = { };
+            int hit = 8;
+            int t = 6;
+            Assert.AreEqual(0, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void OneMonsterNotEnoughTime()
+        {
+            int[] h = { 6 };
+            int hit = 2;
+            int t = 0;
+            Assert.AreEqual(0, FightTheMonsters.KillCount(h, hit, t));
+            t = 1;
+            Assert.AreEqual(0, FightTheMonsters.KillCount(h, hit, t));
+            t = 2;
+            Assert.AreEqual(0, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void OneMonsterJustEnoughTime()
+        {
+            int[] h = { 6 };
+            int hit = 2;
+            int t = 3;
+            Assert.AreEqual(1, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void OneMonsterMoreThanEnoughTime()
+        {
+            int[] h = { 6 };
+            int hit = 2;
+            int t = 4;
+            Assert.AreEqual(1, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void TwoMonstersNotEnoughTimeForEither()
+        {
+            int[] h = { 2, 3 };
+            int hit = 1;
+            int t = 1;
+            Assert.AreEqual(0, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void TwoMonstersEnoughTimeToHitSmallerOnly()
+        {
+            int[] h = { 2, 3 };
+            int hit = 1;
+            int t = 2;
+            Assert.AreEqual(1, FightTheMonsters.KillCount(h, hit, t));
+            t = 3;
+            Assert.AreEqual(1, FightTheMonsters.KillCount(h, hit, t));
+            t = 4;
+            Assert.AreEqual(1, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void TwoMonstersEnoughTimeForBoth()
+        {
+            int[] h = { 2, 3 };
+            int hit = 1;
+            int t = 5;
+            Assert.AreEqual(2, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void ThreeMonstersEnoughTimeForTwo()
+        {
+            int[] h = { 2, 3, 1 };
+            int hit = 2;
+            int t = 2;
+            Assert.AreEqual(2, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void ThreeMonstersEnoughTimeForAll()
+        {
+            int[] h = { 2, 3, 1 };
+            int hit = 2;
+            int t = 4;
+            Assert.AreEqual(3, FightTheMonsters.KillCount(h, hit, t));
+        }
+
+        [TestMethod]
+        public void SampleInput()
+        {
+            int[] h = { 16, 19, 7, 11, 23, 8, 16 };
+            int hit = 8;
+            int t = 6;
+            Assert.AreEqual(4, FightTheMonsters.KillCount(h, hit, t));
+        }
+    }
+}

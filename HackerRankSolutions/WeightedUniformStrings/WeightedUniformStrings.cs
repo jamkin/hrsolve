c3492050e5a2c027cb82e39d2a368095e90bb439
Jamin King <jamintking@gmail.com>
2017-05-16 22:51:01 -0700

    - Wrote solution and verification tests for Weighted Uniform Strings problem

---
+++ HackerRankSolutions/WeightedUniformStrings/WeightedUniformStrings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.WeightedUniformStrings
+{
+    /// <summary>
+    /// Provides a solution to https://www.hackerrank.com/challenges/weighted-uniform-string
+    /// Runtime of the use case is O(n) where is the length of the string
+    /// </summary>
+    public class WeightedUniformStrings
+    {
+        private HashSet<int> UniformWeights = new HashSet<int>();
+
+        public WeightedUniformStrings(string s)
+        {
+            CacheUniformWeights(s);
+        }
+
+        private static IEnumerable<string> UniformSubstrings(string s)
+        {
+            for(int i = 0, j = 0; i < s.Length; i = j)
+            {
+                for(; j < s.Length && s[j] == s[i]; ++j)
+                    yield return s.Substring(i, j - i + 1);
+            }
+        }
+
+        private void CacheUniformWeights(string s)
+        {
+            var uniformSubstrings = UniformSubstrings(s);
+            foreach(string substring in uniformSubstrings)
+            {
+                int weight = CalculateWeight(substring);
+                UniformWeights.Add(weight);
+            }
+        }
+
+        /// <summary>
+        /// Weight query
+        /// </summary>
+        public bool HasUniformWeight(int w) => UniformWeights.Contains(w);
+
+        private static int CalculateWeight(string uni) => ((int)(uni[0] - 'a') + 1) * uni.Length;
+    }
+}
---
+++ HackerRankSolutions/WeightedUniformStrings/WeightedUniformStrings_Test.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.WeightedUniformStrings
+{
+    [TestClass]
+    public class WeightedUniformStrings_Test
+    {
+        [TestMethod]
+        public void EmptyString()
+        {
+            string s = string.Empty;
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsFalse(wus.HasUniformWeight(0));
+            Assert.IsFalse(wus.HasUniformWeight(1));
+            Assert.IsFalse(wus.HasUniformWeight(int.MaxValue));
+        }
+
+        [TestMethod]
+        public void SingleLetterString()
+        {
+            string s = "a";
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsFalse(wus.HasUniformWeight(0));
+            Assert.IsTrue(wus.HasUniformWeight(1));
+            Assert.IsFalse(wus.HasUniformWeight(2));
+            Assert.IsFalse(wus.HasUniformWeight(int.MaxValue));
+        }
+
+        [TestMethod]
+        public void TwoOfSameLettersString()
+        {
+            string s = "bb";
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsFalse(wus.HasUniformWeight(0));
+            Assert.IsFalse(wus.HasUniformWeight(1));
+            Assert.IsTrue(wus.HasUniformWeight(2));
+            Assert.IsFalse(wus.HasUniformWeight(3));
+            Assert.IsTrue(wus.HasUniformWeight(4));
+            Assert.IsFalse(wus.HasUniformWeight(5));
+        }
+
+        [TestMethod]
+        public void TwoDifferentLettersString()
+        {
+            string s = "ab";
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsFalse(wus.HasUniformWeight(0));
+            Assert.IsTrue(wus.HasUniformWeight(1));
+            Assert.IsTrue(wus.HasUniformWeight(2));
+            Assert.IsFalse(wus.HasUniformWeight(3));
+        }
+
+        [TestMethod]
+        public void SandwichTest()
+        {
+            string s = "aba";
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsFalse(wus.HasUniformWeight(0));
+            Assert.IsTrue(wus.HasUniformWeight(1));
+            Assert.IsTrue(wus.HasUniformWeight(2));
+            Assert.IsFalse(wus.HasUniformWeight(3));
+        }
+
+        [TestMethod]
+        public void TwoOfSameFollowedByOther()
+        {
+            string s = "aab";
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsFalse(wus.HasUniformWeight(0));
+            Assert.IsTrue(wus.HasUniformWeight(1));
+            Assert.IsTrue(wus.HasUniformWeight(2));
+            Assert.IsFalse(wus.HasUniformWeight(3));
+            Assert.IsFalse(wus.HasUniformWeight(4));
+        }
+
+        [TestMethod]
+        public void TwoOfSameFollowedByOtherFollowedByFirst()
+        {
+            string s = "aaba";
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsFalse(wus.HasUniformWeight(0));
+            Assert.IsTrue(wus.HasUniformWeight(1));
+            Assert.IsTrue(wus.HasUniformWeight(2));
+            Assert.IsFalse(wus.HasUniformWeight(3));
+            Assert.IsFalse(wus.HasUniformWeight(4));
+        }
+
+        [TestMethod]
+        public void ThreeDifferent()
+        {
+            string s = "abc";
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsFalse(wus.HasUniformWeight(0));
+            Assert.IsTrue(wus.HasUniformWeight(1));
+            Assert.IsTrue(wus.HasUniformWeight(2));
+            Assert.IsTrue(wus.HasUniformWeight(3));
+            Assert.IsFalse(wus.HasUniformWeight(4));
+        }
+
+        [TestMethod]
+        public void SampleInput()
+        {
+            string s = "abccddde";
+            WeightedUniformStrings wus = new WeightedUniformStrings(s);
+            Assert.IsTrue(wus.HasUniformWeight(1));
+            Assert.IsTrue(wus.HasUniformWeight(3));
+            Assert.IsTrue(wus.HasUniformWeight(12));
+            Assert.IsTrue(wus.HasUniformWeight(5));
+            Assert.IsFalse(wus.HasUniformWeight(9));
+            Assert.IsFalse(wus.HasUniformWeight(10));
+        }
+    }
+}

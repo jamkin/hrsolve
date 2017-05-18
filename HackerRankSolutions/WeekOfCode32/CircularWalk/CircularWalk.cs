16da9269dd02f6547fed127fa915fda2c7da0262
Jamin King <jamintking@gmail.com>
2017-05-17 23:10:13 -0700

    not done yet

---
+++ HackerRankSolutions/WeekOfCode32/CircularWalk/CircularWalk.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.WeekOfCode32.CircularWalk
+{
+    public static class CircularWalk
+    {
+        public static int Solve(int n, int s, int t, int R0, int g, int seed, int p)
+        {
+            int[] R = GetR(n, R0, g, seed, p);
+            int d;
+            return (d = ShortestDistance(R, s, t)) < int.MaxValue ? d : -1;
+        }
+
+
+        private static int ShortestDistance(int[] R, int start, int end)
+        {
+            //int[] d = Enumerable.Repeat(int.MaxValue, R.Length).ToArray();
+            int[] d = new int[R.Length];
+            for(int i = 0; i < d.Length; ++i)
+                d[i] = int.MaxValue;
+            d[start] = 0;
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            HashSet<int> visited = new HashSet<int>();
+            while(q.Count > 0)
+            {
+                int cur = q.Dequeue();
+                if(cur == end)
+                {
+                    return d[cur];
+                }
+                IEnumerable<int> nexts = GetNexts(R, cur);
+                foreach(int child in nexts)
+                {
+                    int distFromCur = d[cur] + 1;
+                    if(distFromCur < d[child])
+                    {
+                        d[child] = distFromCur;
+
+                    }
+                    if(!visited.Contains(child))
+                    {
+                        q.Enqueue(child);
+                    }
+                }
+                visited.Add(cur);
+            }
+            return int.MaxValue;
+        }
+
+        private static int[] GetR(int n, int R0, int g, int seed, int p)
+        {
+            int[] R = new int[n];
+            R[0] = R0;
+            for(int i = 1; i < R.Length; ++i)
+            {
+                R[i] = (R[i - 1] * g + seed) % p;
+            }
+            return R;
+        }
+
+        private static IEnumerable<int> GetNexts(int[] R, int i)
+        {
+            for(int j = 1; j <= R[i]; ++j)
+            {
+                int p1 = (i + j) % R.Length;
+                yield return p1;
+
+                int x;
+                int p2 = (x = (i - j) % R.Length) >= 0 ? x : R.Length - (R.Length - x) % R.Length;
+                yield return p2;
+            }
+        }
+    }
+}
---
+++ HackerRankSolutions/WeekOfCode32/CircularWalk/CircularWalk_Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.WeekOfCode32.CircularWalk
+{
+    [TestClass]
+    public class CircularWalk_Test
+    {
+        [TestMethod]
+        public void SampleInput()
+        {
+            int n = 9;
+            int s = 0;
+            int t = 2;
+            int R0 = 1;
+            int g = 3;
+            int seed = 4;
+            int p = 7;
+            Assert.AreEqual(2, CircularWalk.Solve(n, s, t, R0, g, seed, p));
+        }
+
+        [TestMethod]
+        public void SampleInput_ChangeEndToBeginning()
+        {
+            int n = 9;
+            int s = 0;
+            int t = 0;
+            int R0 = 1;
+            int g = 3;
+            int seed = 4;
+            int p = 7;
+            Assert.AreEqual(0, CircularWalk.Solve(n, s, t, R0, g, seed, p));
+        }
+
+        [TestMethod]
+        public void SampleInput_ChangeToFirstMoveReach()
+        {
+            int n = 9;
+            int s = 0;
+            int t = 8;
+            int R0 = 1;
+            int g = 3;
+            int seed = 4;
+            int p = 7;
+            Assert.AreEqual(1, CircularWalk.Solve(n, s, t, R0, g, seed, p));
+        }
+
+        [TestMethod]
+        public void DoesntErrorOut()
+        {
+            Random r = new Random();
+            for(int k = 0; k < 1000; ++k)
+            {
+                int n = r.Next(1, (int)10e7 + 1);
+                int s = r.Next(1, n);
+                int t = r.Next(0, n);
+                int p = r.Next(1, n + 1);
+                int R0 = r.Next(0, p);
+                int g = r.Next(0, p);
+                int seed = r.Next(0, p);
+                int result = CircularWalk.Solve(n, s, t, R0, g, seed, p);
+                Assert.IsTrue(result >= -1 && result < p);
+            }
+        }
+    }
+}

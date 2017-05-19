468432549e828568d9aea162df52339a4eeeee4e
Jamin King <jamintking@gmail.com>
2017-05-19 08:38:23 -0700

    - Created solution to Geometric Trick problem. This times out on the last few   preliminary test cases. - Possible TODO: Optimize

---
+++ HackerRankSolutions/WeekOfCode32/GeometricTrick/GeometricTrick.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.WeekOfCode32.GeometricTrick
+{
+    public static class GeometricTrick
+    {
+        public static readonly char[] Letters = { 'a', 'b', 'c' };
+
+        public static int Solve(string s)
+        {
+            var range = Enumerable.Range(1, s.Length);
+            Dictionary<ulong, int> squareRoots = range.ToDictionary(i => (ulong)i * (ulong)i, i => i);
+            int count = 0;
+            foreach(ulong square in squareRoots.Keys)
+            {
+                var multipliers = Multipliers(square, (ulong)s.Length);
+                foreach(Tuple<int, int> pair in multipliers)
+                {
+                    int i = pair.Item1, k = pair.Item2, j = squareRoots[square];
+                    char[] p1 = new char[] { s[i - 1], s[j - 1], s[k - 1] };
+                    char[] p2 = new char[] { s[k - 1], s[j - 1], s[i - 1] };
+                    if(p1.SequenceEqual(Letters) || p2.SequenceEqual(Letters))
+                    {
+                        ++count;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public static IEnumerable<Tuple<int, int>> Multipliers(ulong m, ulong limit)
+        {
+            ulong i = 1;
+            ulong lastVal = limit;
+            for(; i <= lastVal; i++)
+            {
+                if(m % i == 0)
+                {
+                    lastVal = m / i;
+                    if(lastVal <= limit)
+                    {
+                        yield return Tuple.Create((int)i, (int)lastVal);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HackerRankSolutions/WeekOfCode32/GeometricTrick/GeometricTrick_Test.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HackerRankSolutions.WeekOfCode32.GeometricTrick
+{
+    [TestClass]
+    public class GeometricTrick_Test
+    {
+        [TestMethod]
+        public void EmptyInput()
+        {
+            Assert.AreEqual(0, GeometricTrick.Solve(string.Empty));
+        }
+
+        [TestMethod]
+        public void SampleInput()
+        {
+            Assert.AreEqual(2, GeometricTrick.Solve("ccaccbbbaccccca"));
+        }
+
+        [TestMethod]
+        public void NotEmptyButTooSmal()
+        {
+            Assert.AreEqual(0, GeometricTrick.Solve("ac"));
+        }
+
+        [TestMethod]
+        public void AllThreeLettersNoMore()
+        {
+            Assert.AreEqual(0, GeometricTrick.Solve("abc"));
+        }
+
+        [TestMethod]
+        public void MiscTest_2()
+        {
+            Assert.AreEqual(35, GeometricTrick.Solve("aaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbccccccccccaaaaaaaaaabbbbbbbbbbcccccccccc"));
+        }
+
+        [TestMethod]
+        public void OneOccurrenceWithinLargeString()
+        {
+            char[] ch = Enumerable.Repeat('c', 100).ToArray();
+            ch[1] = 'a';
+            ch[17] = 'b';
+            string s = new string(ch);
+            Assert.AreEqual(1, GeometricTrick.Solve(s));
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void MiscLargestStringPossibleTest()
+        {
+            string s = new string(Enumerable.Repeat('a', (int)5e5 + 1).ToArray());
+            GeometricTrick.Solve(s);
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void RandomValidStringsDoesntErrorOut()
+        {
+            for(int t = 0; t < 100; ++t)
+            {
+                string s = GenerateString();
+                GeometricTrick.Solve(s);
+            }
+        }
+
+        [TestMethod]
+        public void CompareAgainstBruteForce()
+        {
+            for(int t = 0; t < 1000; ++t)
+            {
+                string s = GenerateString(100);
+                int brute = Brute(s);
+                int algo = GeometricTrick.Solve(s);
+                Assert.AreEqual(brute, algo);
+            }
+        }
+
+        private static int Brute(string s)
+        {
+            var charIndexPairs = s.Select((c, i) => new { C = c, J = i + 1 });
+            return (
+                from x in charIndexPairs
+                from y in charIndexPairs
+                from z in charIndexPairs
+                where (x.J * y.J == z.J * z.J)
+                    && new char[] { x.C, z.C, y.C }.SequenceEqual("abc")
+                select 1
+            ).Count();
+        }
+
+        private static char[] StringCharacters = { 'a', 'b', 'c' };
+
+        private static string GenerateString(int maxSize = (int)5e5 + 1)
+        {
+            Random r = new Random();
+            int n = r.Next(1, maxSize);
+            char[] ch = new char[n];
+            for(int i = 0; i < n; ++i)
+                ch[i] = StringCharacters[r.Next(0, StringCharacters.Length)];
+            return new string(ch);
+        }
+    }
+}

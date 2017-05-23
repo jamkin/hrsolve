24bc755bd3c588ebc205d36aa7ab660acdbddcbd
Jamin King <jamintking@gmail.com>
2017-05-23 06:48:41 -0700

    - Created working solution to Marc's Cakewalk problem - Did not write tests. Am no longer writing tests for   every easy problem.

---
+++ HackerRankSolutions/MarcsCakewalk/MarcsCakewalk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.MarcsCakewalk
+{
+    public static class MarcsCakewalk
+    {
+        public static ulong Solve(int[] c)
+        {
+            int[] r = c.OrderByDescending(i => i).ToArray();
+            ulong miles = 0, pow = 1;
+            for(int k = 0; k < r.Length; ++k, pow *= 2) 
+            {
+                miles += (ulong)r[k] * pow;
+            }
+            return miles;
+        }
+    }
+}

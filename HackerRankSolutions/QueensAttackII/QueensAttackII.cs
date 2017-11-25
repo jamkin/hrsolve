68b7a1d92cab80c575ab29dfb342ea5ad2adf25a
Jamin King <jamintking@gmail.com>
2017-11-24 21:15:43 -0800

    Solution and test cases to Queen's Attack II

---
+++ HackerRankSolutions/QueensAttackII/QueensAttackII.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.QueensAttackII
+{
+    /// <summary>
+    /// Provides a solution for https://www.hackerrank.com/challenges/queens-attack-2
+    /// </summary>
+    public class QueensAttackII
+    {
+        public static int Solve(int n, Tuple<int, int> queen, IEnumerable<Tuple<int, int>> obstacles)
+        {
+            var obstacleSet = new HashSet<Tuple<int, int>>(obstacles);
+            int moves = 0;
+            for(int dx = -1; dx <= 1; ++dx)
+            {
+                for(int dy = -1; dy <= 1; ++dy)
+                {
+                    if((dx | dy) == 0)
+                        continue;
+                    for(int x = queen.Item2 + dx, y = queen.Item1 + dy;
+                        x >= 1 && x <= n && y >= 1 && y <= n && !obstacleSet.Contains(new Tuple<int, int>(y, x));
+                        x += dx, y += dy, ++moves) ;
+                }
+            }               
+            return moves;
+        }
+    }
+}
---
+++ HackerRankSolutions/QueensAttackII/QueensAttackII_Test.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HackerRankSolutions.QueensAttackII
+{
+    [TestClass]
+    public class QueensAttackII_Test
+    {
+        [TestMethod]
+        public void Sample_Testcase_0()
+        {
+            int n = 4;
+            Tuple<int, int> queen = Tuple.Create(4, 4);
+            var obstacles = new Tuple<int, int>[] { };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(9, actual);
+        }
+
+        [TestMethod]
+        public void Sample_Testcase_1()
+        {
+            int n = 5;
+            Tuple<int, int> queen = Tuple.Create(4, 3);
+            var obstacles = new Tuple<int, int>[]
+            {
+                Tuple.Create(5, 5),
+                Tuple.Create(4, 2),
+                Tuple.Create(2, 3)
+            };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(10, actual);
+        }
+
+        [TestMethod]
+        public void SingleSpaceNoOstacles()
+        {
+            int n = 1;
+            Tuple<int, int> queen = Tuple.Create(1, 1);
+            var obstacles = new Tuple<int, int>[] { };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void FourSpacesZeroObstacles()
+        {
+            int n = 2;
+            Tuple<int, int> queen = Tuple.Create(2, 2);
+            var obstacles = new Tuple<int, int>[] { };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void FourSpacesOneObstacle()
+        {
+            int n = 2;
+            Tuple<int, int> queen = Tuple.Create(2, 2);
+            var obstacles = new Tuple<int, int>[]
+            {
+                Tuple.Create(1, 1)
+            };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void FourSpacesTwoObstacles()
+        {
+            int n = 2;
+            Tuple<int, int> queen = Tuple.Create(2, 2);
+            var obstacles = new Tuple<int, int>[]
+            {
+                Tuple.Create(1, 1),
+                Tuple.Create(1, 2)
+            };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void FourSpacesThreeObstacles()
+        {
+            int n = 2;
+            Tuple<int, int> queen = Tuple.Create(2, 2);
+            var obstacles = new Tuple<int, int>[]
+            {
+                Tuple.Create(1, 1),
+                Tuple.Create(1, 2),
+                Tuple.Create(2, 1)
+            };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void TwentyFiveSpacesZeroObstacles()
+        {
+            int n = 5;
+            Tuple<int, int> queen = Tuple.Create(3, 3);
+            var obstacles = new Tuple<int, int>[] { };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(16, actual);
+        }
+
+        [TestMethod]
+        public void TwentyFiveSpacesOneEdgeObstacle()
+        {
+            int n = 5;
+            Tuple<int, int> queen = Tuple.Create(3, 3);
+            var obstacles = new Tuple<int, int>[]
+            {
+                Tuple.Create(3, 5)
+            };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(15, actual);
+        }
+
+        [TestMethod]
+        public void TwentyFiveSpacesOneAdjacentObstacle()
+        {
+            int n = 5;
+            Tuple<int, int> queen = Tuple.Create(3, 3);
+            var obstacles = new Tuple<int, int>[]
+            {
+                Tuple.Create(3, 4)
+            };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(14, actual);
+        }
+
+        [TestMethod]
+        public void TwentyFiveSpacesOneNonobstacle()
+        {
+            int n = 5;
+            Tuple<int, int> queen = Tuple.Create(3, 3);
+            var obstacles = new Tuple<int, int>[]
+            {
+                Tuple.Create(1, 4)
+            };
+
+            int actual = QueensAttackII.Solve(n, queen, obstacles);
+
+            Assert.AreEqual(16, actual);
+        }
+    }
+}
